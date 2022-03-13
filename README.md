b48500ee4048c467866be55e08d18f215ddb7825
Roopa Ramanujam <roopa.ramanujam@terraindata.com>
2022-03-13 16:59:19 -0400

    adding additional unit tests - commented out, updating README

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Bulk Address Validation
 
-- Hosted at https://address-validation-api.herokuapp.com/addressValidations
+- Endpoint can be accessed at https://address-validation-api.herokuapp.com/addressValidations
 
 ## Features
 
 - POST endpoint that accepts up to 5 addresses in a JSON array format to receive validated address and latitude/longitude coordinates
 - Caching mechanism for frequently looked up addresses
+- Hosted on the cloud
 
 ## Request
 
@@ -25,7 +26,7 @@ Array of JSON object addresses with the keys "address_line_one", "city", "state"
 
 ## Response
 
-HTTP 207 Multi-part response with the status and response body for each address in the request.
+HTTP 207 Multi-part response with the status and response body for each address in the request. See example below for error status examples for each address entry.
 
 ```json
 [
@@ -55,6 +56,8 @@ HTTP 207 Multi-part response with the status and response body for each address 
 ]
 ```
 
+The API will return an HTTP 400 error for the whole request if the addresses are not passed as an array, if the array is empty, or there are more than 5 entries.
+
 ## Dependencies:
 
 1. Google GeoCoding API for address validation and geolocation
@@ -72,4 +75,4 @@ HTTP 207 Multi-part response with the status and response body for each address 
 
 1. Further preprocessing on request address to ensure that cache lookup would match despite differences in spelling or syntax. Basic mapping of most common standardizations for routes and states available for now.
 2. Evict entries from cache after certain period of time
-3. Additional unit tests
+3. Fix commented out unit tests
--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ import sinon from "sinon";
 import proxyquire from "proxyquire";
 import chaiHttp from "chai-http";
 
+import * as addressCache from "../cache/addressCache.js";
 import { cacheLookup, cacheInsert } from "../cache/addressCache.js";
 import { validateAddress } from "../api/controllers/addressValidations.js";
 
@@ -11,7 +12,7 @@ describe("Bulk address validation endpoint request validation", () => {
   it("should return http status code 207 - multipart response - if the overall request is valid", async () => {
     let addresses = [
       {
-        address_line_one: "620 Atlantic Ave",
+        address_line_one: "620 Atlantic Avenue",
         city: "Brooklyn",
         state: "NY",
         zip_code: "11217",
@@ -117,13 +118,11 @@ describe("Bulk address validation endpoint request validation", () => {
 });
 
 describe("Bulk address validation endpoint response validation", () => {
-  // need to mock/stub functions from addressValidations and addressCache to unit test the rest of the validation endpoint
+  // need to fix mocking/stubbing functions from addressValidations and addressCache to unit test the rest of the validation endpoint
   // test cases:
-  // should not call validateAddress or cacheInsert if cacheLookup return value is not null
+  // should not call validateAddress or cacheInsert if cacheLookup returns non-null value
   // should call validateAddress and cacheInsert if cacheLookup return value is null
-  // should return 404 in multipart response if validateAddress call returns empty
-  // should return 200 in multipart response if validateAddress call returns object
-  // should return multipart response matching number of requests sent in array
+  // should return multipart response matching number of requests sent in array with success or error messages
   // it("should not call validateAddress or cacheInsert if cacheLookup return value is not null", async () => {
   //   let addresses = [
   //     {
@@ -134,17 +133,17 @@ describe("Bulk address validation endpoint response validation", () => {
   //     },
   //   ];
   //   chai.use(chaiHttp);
-  //   const validateAddressSpy = sinon.spy(validateAddressSpy);
+  //   const validateAddressSpy = sinon.spy(validateAddress);
   //   const cacheInsertSpy = sinon.spy(cacheInsert);
-  //   sinon.stub(addressCache, 'cacheLookup').returns({
+  //   sinon.stub(addressCache, "cacheLookup").returns({
   //     id: "someId",
   //     address_line_one: "620 Atlantic Ave",
   //     city: "Brooklyn",
   //     state: "NY",
   //     zip_code: "11217",
   //     latitude: "42.00",
-  //     longitude: "73.00"
-  //   })
+  //     longitude: "73.00",
+  //   });
   //   const res = await chai
   //     .request(app)
   //     .post("../api/controllers/addressValidations.js")
@@ -152,5 +151,26 @@ describe("Bulk address validation endpoint response validation", () => {
   //   expect(res).to.have.status(207);
   //   expect(validateAddressSpy).not.to.be.called();
   //   expect(cacheInsertSpy).not.to.be.called();
+  // });
+  // it("should call validateAddress and cacheInsert if cacheLookup return value is null", async () => {
+  //   let addresses = [
+  //     {
+  //       address_line_one: "620 Atlantic Ave",
+  //       city: "Brooklyn",
+  //       state: "NY",
+  //       zip_code: "11217",
+  //     },
+  //   ];
+  //   chai.use(chaiHttp);
+  //   const validateAddressSpy = sinon.spy(validateAddress);
+  //   const cacheInsertSpy = sinon.spy(cacheInsert);
+  //   sinon.stub(addressCache, "cacheLookup").returns(null);
+  //   const res = await chai
+  //     .request(app)
+  //     .post("../api/controllers/addressValidations.js")
+  //     .send(addresses);
+  //   expect(res).to.have.status(207);
+  //   expect(validateAddressSpy).to.be.called();
+  //   expect(cacheInsertSpy).to.be.called();
   // });
 });
